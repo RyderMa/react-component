38adf0bb5edddd9d38461ca42e28144d67a8c9cf
mlj_1 <17862835855@163.com>
2021-01-13 18:44:15 +0800

    可搜索组合

---
+++ src/components/Input/search.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+// import classnames from "classnames";
+import Button from "../Button/button";
+import Input, { InputProps } from "./input";
+
+export interface SearchProps extends InputProps {
+  clearAble?: boolean;
+  searchEle?: string | React.ReactNode;
+  onSearch?: (event?: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+}
+
+export const Search: FC<SearchProps> = (props) => {
+  const { clearAble, searchEle, onSearch } = props;
+  return (
+    <span className="mantd-input-group-wrapper">
+      <span className="mantd-input-wrapper mantd-input-group">
+        <Input></Input>
+        <Button btnType="primary" onClick={() => onSearch && onSearch()}>
+          {searchEle ? searchEle : "搜索"}
+        </Button>
+      </span>
+    </span>
+  );
+};
+
+export default Search;
